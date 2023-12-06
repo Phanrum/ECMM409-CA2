5fe5d7b10fdc34b46637503b967b3daf8033b77a
meitt8 <92991931+meitt8@users.noreply.github.com>
2023-12-06 08:04:27 +0000

    mostly docs and tried to use generate_cities_and_items in the pseudocode notebook

--- generate_cities_and_items_sanj.py
+++ generate_cities_and_items_sanj.py
@@ -2,27 +2,36 @@ from io import StringIO
 import pandas as pd
 import numpy as np
 
-def generate_cities_and_items(Q):
+def generate_cities_and_items(Q, file):
   """
   This function generates and returns a random order in which cities are to be traversed,
   along with a binary array that decides which items must be put in the knapsack
 
-  parameters:
-  1. Q: int
-  Accepts the total capacity of the knapsack
+  Parameters
+  ----------
+  Q : int
+    The total capacity of the knapsack
+  file : str
+    The path to the dataset file
 
-  returns:
-  1. city_travel: int
-  Returns the order of city traversal
-  2. items_select: int
-  Returns the binary array that decides which items muust be picked
+
+  Returns
+  -------
+  city_travel : np.array[int]
+    The order of cities to be visited
+  items_selected : np.array[int]
+    A binary array which decides which items are to be picked
+
   """
-  dataset_file = open('a280-n279.txt', 'r').read()
+
+  # reading the file
+  dataset_file = open(file, 'r').read()
   heading1 = 'NODE_COORD_SECTION	(INDEX, X, Y): '
   heading2 = 'ITEMS SECTION	(INDEX, PROFIT, WEIGHT, ASSIGNED NODE NUMBER): '
   pos1 = dataset_file.find(heading1)+len(heading1)
   pos2 = dataset_file.find(heading2)+len(heading2)
 
+  # unpacking the data
   cities_dataset = pd.read_csv(StringIO(dataset_file[pos1:pos2-len(heading2)]), sep='\t')
   cities_dataset.columns = ['INDEX', 'X', 'Y']
   items_dataset = pd.read_csv(StringIO(dataset_file[pos2:]), sep='\t')
@@ -31,6 +40,8 @@ def generate_cities_and_items(Q):
   city_travel = np.random.choice(cities_dataset['INDEX'], size=len(cities_dataset), replace=False)
   weight_array = np.array(items_dataset['WEIGHT'])
   prob = Q/sum(weight_array)
+
+  ####
   not_a_good_list = True
   while(not_a_good_list):
     w = 0
