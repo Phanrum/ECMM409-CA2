a82e31d27fbcffb32d09586efe96d38cf3ef5382
niiiiiiiiik <nikhilyadav1809@gmail.com>
2023-12-07 19:13:01 +0000

    adding documentatio

--- src/time_function.py
+++ src/time_function.py
@@ -20,65 +20,53 @@ def generate_weight_profit_velocity(vmax, vmin, weights, profits, curr_wt_of_kns
     return None  # Return None if no valid combination is found
 
 def calculate_travel_time(cities, distance_matrix, vmax, vmin, weights, profits, Q):
+    """
+    Calculates the total travel time and profit for a given route through cities, taking into account 
+    the variation in velocity due to changes in the weight of the knapsack.
+
+    Parameters:
+    cities (list of str): List of cities in the order they will be visited.
+    distance_matrix (dict): 2D dictionary with distances between each pair of cities.
+    vmax (float): Maximum velocity of the thief.
+    vmin (float): Minimum velocity of the thief.
+    weights (dict): Dictionary with weights of items in each city.
+    profits (dict): Dictionary with profits of items in each city.
+    Q (float): Maximum capacity of the knapsack.
+
+    Returns:
+    tuple: A tuple of total travel time and total profit for the route.
+    """
+
+    # Validate the input parameters
     if vmax <= 0 or vmin <= 0 or vmax < vmin:
         raise ValueError("Invalid velocities: vmax and vmin must be positive, and vmax must be greater than vmin.")
     if not cities or not distance_matrix:
         raise ValueError("Cities and distance matrix must not be empty.")
 
+    # Initialize the variables for total travel time, current knapsack weight, profit, and velocity
     total_time, curr_wt_of_kns, curr_pro_of_kns, velocity = 0, 0, 0, vmin
 
+    # Iterate through each city in the route
     for i in range(len(cities) - 1):
+        # Determine the weight, profit, and velocity for the current city
         result = generate_weight_profit_velocity(vmax, vmin, weights[cities[i]], profits[cities[i]], curr_wt_of_kns, curr_pro_of_kns, Q)
+        # If a valid combination is found, update the knapsack and velocity
         if result:
             curr_wt_of_kns, curr_pro_of_kns, velocity = result
         else:
-            break  # Break the loop if no valid combination is found
+            # If no valid combination is found, exit the loop
+            break
 
+        # Calculate the time to travel to the next city and update the total travel time
         time_to_next_city = distance_matrix[cities[i]][cities[i+1]] / velocity
         total_time += time_to_next_city
 
-    if result:  # Check if the last iteration was successful
+    # If the last city processed successfully, calculate the time to return to the starting city
+    if result:
         time_to_start_city = distance_matrix[cities[-1]][cities[0]] / velocity
         total_time += time_to_start_city
 
+    # The final total profit is the current profit in the knapsack
     total_profit = curr_pro_of_kns
+
     return total_time, total_profit
-
-# Example usage:
-# Define the cities and the order in which they will be visited.
-#cities = ['A', 'B', 'C', 'D']
-
-# Define the distance matrix.
-#distance_matrix = {
-    'A': {'B': 100, 'C': 250, 'D': 120}#,
-    'B': {'A': 100, 'C': 150, 'D': 350}#,
-    'C': {'A': 250, 'B': 150, 'D': 200}#,
-    'D': {'A': 120, 'B': 350, 'C': 200}
-#}
-
-# Define the weights and profits for each city.
-#weights = {
-    'A': [1, 2]#,
-    'B': [2, 3]#,
-    'C': [3, 4]#,
-    'D': [4, 5]
-#}
-#profits = {
-    'A': [10, 20]#,
-    'B': [20, 30]#,
-    'C': [30, 40]#,
-    'D': [40, 50]
-#}
-
-# Knapsack capacity
-#Q = 5
-
-# Velocities
-#vmax = 100
-#vmin = 50
-
-# Calculate the travel time and profit.
-#total_time, total_profit = calculate_travel_time(cities, distance_matrix, vmax, vmin, weights, profits, Q)
-
-# Output the results.
-#print(total_time, total_profit)
