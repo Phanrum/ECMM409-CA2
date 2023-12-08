5da7673d17f6c39bd844891bc20b170d85545d16
meitt8 <92991931+meitt8@users.noreply.github.com>
2023-12-08 13:51:07 +0000

    cleaned it up dw

--- src/generate_cities_and_items_sanj.py
+++ src/generate_cities_and_items_sanj.py
@@ -10,8 +10,10 @@ def generate_cities_and_items_random(dataset, item_section):
 
   Parameters
   ----------
-
-
+  dataset : parsing.Dataset
+    Parsed data.
+  item_section : 2D numpy array
+    A reconstruction of the item section from the parsed data.
 
   Returns
   -------
@@ -26,22 +28,6 @@ def generate_cities_and_items_random(dataset, item_section):
     Q = dataset.knapsack_capacity
     number_of_cities = dataset.dimension
     city_indices = [dataset.nodes[i].index for i in range(number_of_cities)]
-
-    # dataset_file = open(file, 'r').read()
-    # heading1 = 'NODE_COORD_SECTION	(INDEX, X, Y): '
-    #
-    # heading2 = 'ITEMS SECTION	(INDEX, PROFIT, WEIGHT, ASSIGNED NODE NUMBER): '
-    # pos1 = dataset_file.find(heading1) + len(heading1)
-    # pos2 = dataset_file.find(heading2) + len(heading2)
-    #
-    # # unpacking the data
-    # cities_dataset = pd.read_csv(StringIO(dataset_file[pos1:pos2 - len(heading2)]), sep='\t')
-    # cities_dataset.columns = ['INDEX', 'X', 'Y']
-
-
-
-    # items_dataset = pd.read_csv(StringIO(dataset_file[pos2:]), sep='\t')
-    # items_dataset.columns = ['INDEX', 'PROFIT', 'WEIGHT', 'ASSIGNED NODE NUMBER']
 
     # create a list of cities to visit
     city_travel = np.random.choice(city_indices, size=number_of_cities, replace=False)
